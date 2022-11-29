96f634040e3ac8e8438ba2f447c4e86280013703
rajeevan <naveejr@gmail.com>
2022-11-30 00:39:59 +0530

    Cards module impl

---
+++ Microservices/cards/src/main/java/com/naveejr/cards/controller/CardsController.java
@@ -0,0 +1,32 @@
+package com.naveejr.cards.controller;
+
+
+import com.naveejr.cards.model.Cards;
+import com.naveejr.cards.model.Customer;
+import com.naveejr.cards.repository.CardsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class CardsController {
+
+
+	private final CardsRepository cardsRepository;
+
+	@PostMapping("/myCards")
+	public List<Cards> getCardDetails(@RequestBody Customer customer) {
+		List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+		if (cards != null) {
+			return cards;
+		} else {
+			return null;
+		}
+
+	}
+
+}
---
+++ Microservices/cards/src/main/java/com/naveejr/cards/repository/CardsRepository.java
@@ -0,0 +1,15 @@
+package com.naveejr.cards.repository;
+
+import java.util.List;
+
+import com.naveejr.cards.model.Cards;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardsRepository extends CrudRepository<Cards, Long> {
+
+
+	List<Cards> findByCustomerId(int customerId);
+
+}
