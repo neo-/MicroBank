3c6949988d4d2ed1f5c53f67db7548d8db743ddf
rajeevan <naveejr@gmail.com>
2022-11-30 00:34:03 +0530

    Loans module impl

---
+++ Microservices/loans/src/main/java/com/naveejr/loans/controller/LoansController.java
@@ -0,0 +1,30 @@
+package com.naveejr.loans.controller;
+
+import com.naveejr.loans.model.Customer;
+import com.naveejr.loans.model.Loans;
+import com.naveejr.loans.repository.LoansRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class LoansController {
+
+	private final LoansRepository loansRepository;
+
+	@PostMapping("/myLoans")
+	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+		List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+		if (loans != null) {
+			return loans;
+		} else {
+			return null;
+		}
+
+	}
+
+}
