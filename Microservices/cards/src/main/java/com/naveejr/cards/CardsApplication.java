7fca5a5b5d7bbacddd96472696d166f2a812ece8
rajeevan <naveejr@gmail.com>
2022-11-29 23:53:08 +0530

    initial

--- Microservices/cards/src/main/java/com/naveejr/cards/CardsApplication.java
+++ Microservices/cards/src/main/java/com/naveejr/cards/CardsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CardsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
 		SpringApplication.run(CardsApplication.class, args);
 	}
 
