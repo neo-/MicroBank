2990f5d416e8ca5157da11fede6900769455bf64
rajeevan <naveejr@gmail.com>
2022-12-10 21:34:46 +0530

    Docker compose update for gateway server

--- docker_build.sh
+++ docker_build.sh
@@ -6,3 +6,10 @@ mvn -f Microservices/config-server/pom.xml spring-boot:build-image -DskipTests
 mvn -f Microservices/eureka-server/pom.xml spring-boot:build-image -DskipTests
 mvn -f Microservices/gateway-server/pom.xml spring-boot:build-image -DskipTests
 mvn -f Microservices/loans/pom.xml spring-boot:build-image -DskipTests
+
+docker push naveejr/accounts
+docker push naveejr/cards
+docker push naveejr/config-server
+docker push naveejr/eureka-server
+docker push naveejr/gateway-server
+docker push naveejr/loans
