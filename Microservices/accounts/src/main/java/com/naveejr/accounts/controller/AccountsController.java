ad8296a19c86a9b77f45fd3f749c2c628e3ac6fa
rajeevan <naveejr@gmail.com>
2022-12-12 01:09:46 +0530

    MicroMeter and Prometheus

--- Microservices/accounts/src/main/java/com/naveejr/accounts/controller/AccountsController.java
+++ Microservices/accounts/src/main/java/com/naveejr/accounts/controller/AccountsController.java
@@ -9,6 +9,7 @@ import com.naveejr.accounts.repository.AccountsRepository;
 import com.naveejr.accounts.service.client.CardsFeignClient;
 import com.naveejr.accounts.service.client.LoansFeignClient;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,45 +26,46 @@ import java.util.List;
 @RestController
 public class AccountsController {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(AccountsController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccountsController.class);
 
-	private final AccountsServiceConfig accountsServiceConfig;
-	private final AccountsRepository accountsRepository;
+    private final AccountsServiceConfig accountsServiceConfig;
+    private final AccountsRepository accountsRepository;
 
-	private final LoansFeignClient loansFeignClient;
+    private final LoansFeignClient loansFeignClient;
 
-	private final CardsFeignClient cardsFeignClient;
+    private final CardsFeignClient cardsFeignClient;
 
-	@PostMapping("/myAccount")
-	public Accounts getAccountDetails(@RequestBody Customer customer) {
-		return accountsRepository.findByCustomerId(customer.getCustomerId());
-	}
+    @PostMapping("/myAccount")
+    @Timed(value = "getAccountDetails.time", description = "Time taken to return Account Details")
+    public Accounts getAccountDetails(@RequestBody Customer customer) {
+        return accountsRepository.findByCustomerId(customer.getCustomerId());
+    }
 
-	@PostMapping("/myCustomerDetails")
-	@CircuitBreaker(name = "detailsForCustomerSupportApp")
-	public CustomerDetails getCustomerDetails(@RequestBody Customer customer) {
-		LOGGER.info("Getting customer details for {}", customer.getCustomerId());
-		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
-		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
-		List<Cards> cards = cardsFeignClient.getCardDetails(customer);
-		CustomerDetails customerDetails = new CustomerDetails();
-		customerDetails.setAccounts(accounts);
-		customerDetails.setLoans(loans);
-		customerDetails.setCards(cards);
-		return customerDetails;
-	}
+    @PostMapping("/myCustomerDetails")
+    @CircuitBreaker(name = "detailsForCustomerSupportApp")
+    public CustomerDetails getCustomerDetails(@RequestBody Customer customer) {
+        LOGGER.info("Getting customer details for {}", customer.getCustomerId());
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+        List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+        List<Cards> cards = cardsFeignClient.getCardDetails(customer);
+        CustomerDetails customerDetails = new CustomerDetails();
+        customerDetails.setAccounts(accounts);
+        customerDetails.setLoans(loans);
+        customerDetails.setCards(cards);
+        return customerDetails;
+    }
 
-	@GetMapping("account/properties")
-	public ResponseEntity<Properties> getAccountServiceConfig() {
+    @GetMapping("account/properties")
+    public ResponseEntity<Properties> getAccountServiceConfig() {
 
-		LOGGER.info("Getting customer details ");
-		return ResponseEntity.ok(
-				new Properties(
-						accountsServiceConfig.getMsg(),
-						accountsServiceConfig.getBuildVersion(),
-						accountsServiceConfig.getMailDetails(),
-						accountsServiceConfig.getActiveBranches())
-		);
-	}
+        LOGGER.info("Getting customer details ");
+        return ResponseEntity.ok(
+                new Properties(
+                        accountsServiceConfig.getMsg(),
+                        accountsServiceConfig.getBuildVersion(),
+                        accountsServiceConfig.getMailDetails(),
+                        accountsServiceConfig.getActiveBranches())
+        );
+    }
 
 }
